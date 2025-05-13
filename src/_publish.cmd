5e23cd6d95156f10727b2ee8b8c467cf02646cb8
ted-sharp <ted.sharp.flat@gmail.com>
2025-05-14 00:24:46 +0900

    リリース用スクリプトを修正

--- src/_publish.cmd
+++ src/_publish.cmd
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 cd /d %~dp0
 
-echo Publishing Aloe.Utils.Wafu.JisCompat...
+echo Publishing Aloe.Utils.Wafu.Romaji...
 
 rem Clean previous publish directory
 if exist "publish" (
@@ -13,15 +13,15 @@ if exist "publish" (
 
 rem Build the project
 echo Building the project...
-dotnet build .\Aloe.Utils.Wafu.JisCompat\Aloe.Utils.Wafu.JisCompat.csproj -c Release
+dotnet build .\Aloe.Utils.Wafu.Romaji\Aloe.Utils.Wafu.Romaji.csproj -c Release
 
 rem Publish the application
 echo Building and publishing...
-dotnet publish .\Aloe.Utils.Wafu.JisCompat\Aloe.Utils.Wafu.JisCompat.csproj -c Release -r win-x64 -o .\publish\AloeUtilsWafuJisCompat
+dotnet publish .\Aloe.Utils.Wafu.Romaji\Aloe.Utils.Wafu.Romaji.csproj -c Release -r win-x64 -o .\publish\AloeUtilsWafuRomaji
 
 rem Create NuGet package
 echo Creating NuGet package...
-dotnet pack .\Aloe.Utils.Wafu.JisCompat\Aloe.Utils.Wafu.JisCompat.csproj -c Release -o .\publish
+dotnet pack .\Aloe.Utils.Wafu.Romaji\Aloe.Utils.Wafu.Romaji.csproj -c Release -o .\publish
 
 if %ERRORLEVEL% EQU 0 (
     echo.
