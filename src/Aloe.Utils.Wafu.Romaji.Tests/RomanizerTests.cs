28ab303f4b6d679e425b46e508bad647b7fb79e7
ted-sharp <ted.sharp.flat@gmail.com>
2025-05-14 22:08:07 +0900

    テストを修正

--- src/Aloe.Utils.Wafu.Romaji.Tests/RomanizerTests.cs
+++ src/Aloe.Utils.Wafu.Romaji.Tests/RomanizerTests.cs
@@ -2,6 +2,8 @@
 
 using Xunit;
 using Aloe.Utils.Wafu.Romaji;
+using Xunit.Abstractions;
+using System.ComponentModel.DataAnnotations;
 
 /// <summary>
 /// Romanizer クラスのテスト
@@ -20,7 +22,8 @@ public class RomanizerTests
     [InlineData("やゆよ", "yayuyo")]
     [InlineData("らりるれろ", "rarirurero")]
     [InlineData("わをん", "wawon")]
-    public void Convert_基本文字_正しく変換される(string input, string expected)
+    [Display(Name = "基本文字が正しく変換されること")]
+    public void Convert_BasicCharacters_ConvertsCorrectly(string input, string expected)
     {
         // Arrange & Act
         var actual = Romanizer.Convert(input);
@@ -35,7 +38,8 @@ public class RomanizerTests
     [InlineData("だぢづでど", "dajizudedo")]
     [InlineData("ばびぶべぼ", "babibubebo")]
     [InlineData("ぱぴぷぺぽ", "papipupepo")]
-    public void Convert_濁音半濁音_正しく変換される(string input, string expected)
+    [Display(Name = "濁音・半濁音が正しく変換されること")]
+    public void Convert_DakutenAndHandakuten_ConvertsCorrectly(string input, string expected)
     {
         // Arrange & Act
         var actual = Romanizer.Convert(input);
@@ -56,7 +60,8 @@ public class RomanizerTests
     [InlineData("じゃじゅじょ", "jajujo")]
     [InlineData("びゃびゅびょ", "byabyubyo")]
     [InlineData("ぴゃぴゅぴょ", "pyapyupyo")]
-    public void Convert_拗音_正しく変換される(string input, string expected)
+    [Display(Name = "拗音が正しく変換されること")]
+    public void Convert_Yoon_ConvertsCorrectly(string input, string expected)
     {
         // Arrange & Act
         var actual = Romanizer.Convert(input);
@@ -81,7 +86,8 @@ public class RomanizerTests
     [InlineData("っつ", "ttsu")]
     [InlineData("って", "tte")]
     [InlineData("っと", "tto")]
-    public void Convert_促音_正しく変換される(string input, string expected)
+    [Display(Name = "促音が正しく変換されること")]
+    public void Convert_Sokuon_ConvertsCorrectly(string input, string expected)
     {
         // Arrange & Act
         var actual = Romanizer.Convert(input);
@@ -96,7 +102,8 @@ public class RomanizerTests
     [InlineData("うー", "uu")]
     [InlineData("えー", "ee")]
     [InlineData("おー", "oo")]
-    public void Convert_長音_正しく変換される(string input, string expected)
+    [Display(Name = "長音が正しく変換されること")]
+    public void Convert_LongVowel_ConvertsCorrectly(string input, string expected)
     {
         // Arrange & Act
         var actual = Romanizer.Convert(input);
@@ -111,7 +118,8 @@ public class RomanizerTests
     [InlineData("うぅ", "uu")]
     [InlineData("えぇ", "ee")]
     [InlineData("おぉ", "oo")]
-    public void Convert_小文字_正しく変換される(string input, string expected)
+    [Display(Name = "小文字が正しく変換されること")]
+    public void Convert_SmallCharacters_ConvertsCorrectly(string input, string expected)
     {
         // Arrange & Act
         var actual = Romanizer.Convert(input);
@@ -131,7 +139,8 @@ public class RomanizerTests
     [InlineData("ク", "ku")]
     [InlineData("ケ", "ke")]
     [InlineData("コ", "ko")]
-    public void Convert_カタカナ_正しく変換される(string input, string expected)
+    [Display(Name = "カタカナが正しく変換されること")]
+    public void Convert_Katakana_ConvertsCorrectly(string input, string expected)
     {
         // Arrange & Act
         var actual = Romanizer.Convert(input);
@@ -141,11 +150,14 @@ public class RomanizerTests
     }
 
     [Theory]
-    [InlineData("あい", "ai")]
-    [InlineData("うえお", "ueo")]
-    [InlineData("かきく", "kakiku")]
-    [InlineData("さしすせそ", "sashisuseso")]
-    public void Convert_複合文字_正しく変換される(string input, string expected)
+    [InlineData("あい123", "ai123")]
+    [InlineData("カタカナ123", "katakana123")]
+    [InlineData("あい うえ", "ai ue")]
+    [InlineData("あい\nうえ", "ai\nue")]
+    [InlineData(null, "")]
+    [InlineData("", "")]
+    [Display(Name = "特殊文字が正しく変換されること")]
+    public void Convert_SpecialCharacters_ConvertsCorrectly(string? input, string expected)
     {
         // Arrange & Act
         var actual = Romanizer.Convert(input);
